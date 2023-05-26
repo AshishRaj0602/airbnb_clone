033e7928a605d174a3581b8926c803164d28cf72
Ashish Rajpoot <115613437+AshishRaj0602@users.noreply.github.com>
2023-05-26 14:41:30 +0530

    comm

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Card.css'
 import { useNavigate } from 'react-router-dom'
 const Card = ({src,title,discription,price}) => {
